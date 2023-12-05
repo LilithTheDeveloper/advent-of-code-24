7a2ee28d11dd2b040f426ea29192618e9830eea0
Vakyria <lilithsgrave@outlook.de>
2023-12-05 13:36:03 +0100

    wow that took way too long

---
+++ 4/Scratchpad_b.py
@@ -0,0 +1,70 @@
+import pprint as pp
+def input():
+
+    with open('input.txt') as f:
+        lines = f.readlines()
+    return list(map(lambda x: x.strip(), lines))
+
+cards = input()
+
+def split_card(idx, card):
+    prefix = card.split(':')[0].split(' ')[1]    
+    card = card.split(':')[1].strip()
+
+    winning_numbers = card.split('|')[0].strip().split()
+    winning_numbers = list(map(lambda x: int(x), winning_numbers))
+
+    scratch_numbers = card.split('|')[1].strip().split()
+    scratch_numbers = list(map(lambda x: int(x), scratch_numbers))
+
+    matching_numbers = 0
+
+    for i in range(0, len(scratch_numbers)):
+        if scratch_numbers[i] in winning_numbers:
+            matching_numbers += 1
+
+    return matching_numbers 
+
+def get_prefix(card):
+    prefix = card.split(':')[0].split()
+    prefix = prefix[1]
+    return prefix
+
+# def card_copier(cards):
+#     cards_copied = [] 
+#     total_wins = 0
+
+#     for i in range(0, len(cards)):
+#         prefix = int(get_prefix(cards[i]))
+#         first = True
+#         while(first or prefix in cards_copied):
+#             wins = split_card(i, cards[i])
+#             if(wins == 0):
+#                 cards = [x for x in cards if x != prefix]
+#                 break
+#             total_wins += wins
+#             cards_copied.extend([prefix+j for j in range(1, wins)])
+#             first = False
+#             if(prefix in cards_copied):
+#                 cards_copied.remove(prefix)
+
+#     print(f"Total wins: {total_wins}")
+#     return cards_copied
+
+# card_copier(cards)
+
+winners = []
+stored_cards = [1 for i in range(0, len(cards))]
+
+for i, card in enumerate(cards):
+    winners.append(split_card(i, card))
+
+for i in range(0, len(winners)):
+    cards_copied = []
+    for j in range(1, winners[i]+1):
+        current_card_amount = stored_cards[i]
+        stored_cards[i+j] += current_card_amount
+        cards_copied.append(i+j+1)
+    # print(f"Card {i+1} has won the cards {cards_copied}. (Winning numbers: {winners[i]})")
+
+print(sum(stored_cards))
